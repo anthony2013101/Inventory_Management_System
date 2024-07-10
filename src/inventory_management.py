86f9b5b3d9892ce2c671247a8c7b509547893a20
anthony2013101 <anthony2013101@g.ucla.edu>
2024-07-10 14:28:29 +0800

    Update inventory_management.py

--- src/inventory_management.py
+++ src/inventory_management.py
@@ -7,8 +7,25 @@ class InventoryManagement:
         self.products = []
 
     # Implement inventory management methods here
+    def create_category(self, category_id, name):
+        category = Category(category_id, name)
+        self.categories.append(name)
+        return category
 
-    def add_product(self, product):
+    def create_product(self, product_id, name, price, quantity, category):
+        product = Product(product_id, name, price, quantity)
+        product.category = category
+        category.add_product(product)
         self.products.append(product)
+        return product
 
-    # Add more inventory management methods as needed
+    def remove_product(self, product):
+        if product not in self.products:
+            raise ValueError("There is no {product} found in the Inventory.")
+        self.products.remove(product)
+        return f'{product} has been removed from Inventory.'
+
+
+inventory_manager = InventoryManagement()
+anthony = inventory_manager.create_category(1234, 'Anthony')
+print(anthony.name)
