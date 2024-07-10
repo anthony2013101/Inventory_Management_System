605c21f30c689055f4db694fd3d601798b6cb804
anthony2013101 <anthony2013101@g.ucla.edu>
2024-07-10 14:28:27 +0800

    Update category.py

--- src/category.py
+++ src/category.py
@@ -5,3 +5,14 @@ class Category:
         self.products = []
 
     # Implement additional category-related methods here
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def remove_product(self, product):
+        self.products.remove(product)
+
+    def list_products(self):
+        return self.products
+
+
