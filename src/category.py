580b25272a46cc4aa859b3193a707384fc77bbd9
Zoe Mak <zoemak117@gmail.com>
2024-07-11 22:29:27 +0800

    Update category.py

--- src/category.py
+++ src/category.py
@@ -1,7 +1,23 @@
+from database import get_connections
 class Category:
-    def __init__(self, category_id, name):
+    def __init__(self, category_id, category_name):
         self.category_id = category_id
-        self.name = name
+        self.category_name = category_name
         self.products = []
 
     # Implement additional category-related methods here
+    def __str__(self):
+        return f"{self.category_name} - Price: ${self.price} - Quantity: {self.quantity}"
+
+    def get_all_categories():
+        connection = get_connections()
+        cursor = connection.cursor()
+        query = """ SELECT category_id, category_name, SUM(quantity) 
+                    FROM inventory
+                    GROUP BY category_id, category_name;"""
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+
+categorylist = [{'category_id':item[0], 'category_name': item[1], 'quantity': item[2]} for item in Category.get_all_categories()]
+print(categorylist)
