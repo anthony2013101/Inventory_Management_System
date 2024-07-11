c3b46d2d678c2ba79c5958863b9855e788b1726f
Zoe Mak <zoemak117@gmail.com>
2024-07-11 21:16:19 +0800

    Update product.py

--- src/product.py
+++ src/product.py
@@ -7,3 +7,13 @@ class Product:
         self.category = None
 
     # Implement additional product-related methods here
+    def all_products():
+        connection = get_connections()
+        cursor = connection.cursor()
+        cursor.execute("""SELECT product_id, product_name, price, quantity FROM inventory;""")
+        result = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return result
+
+    print(all_products())
