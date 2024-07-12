c2d5703d5d432d3a36ebfc4c921ba9981048ba0c
anthony2013101 <anthony2013101@g.ucla.edu>
2024-07-12 20:52:12 +0800

    Update product.py

--- src/product.py
+++ src/product.py
@@ -1,11 +1,20 @@
 class Product:
-    def __init__(self, product_id, name, price, quantity):
+    def __init__(self, product_id, name, price, quantity, category_id = None):
         self.product_id = product_id
         self.name = name
         self.price = price
         self.quantity = quantity
-        self.category = None
+        self.category_id = category_id
 
-    # Implement additional product-related methods here
+    def __str__(self):
+        return f"{self.name} - Price: ${self.price} - Quantity: {self.quantity}"
 
-    print("hello world")
+def get_all_products(connection):
+    product_list = []
+    cursor = connection.cursor()
+    query = """ SELECT * FROM products"""
+    cursor.execute(query)
+    columns = [column[0] for column in cursor.description]
+    for product in cursor.fetchall():
+        product_list.append(Product(**dict(zip(columns, product))))
+    return product_list
