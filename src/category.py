92a9db1010d0ffd11315ec70e9ab3cf11d8f81dc
anthony2013101 <anthony2013101@g.ucla.edu>
2024-07-12 20:51:40 +0800

    Update category.py

--- src/category.py
+++ src/category.py
@@ -4,4 +4,20 @@ class Category:
         self.name = name
         self.products = []
 
-    # Implement additional category-related methods here
+    def add_product(self, product):
+        self.products.append(product)
+
+    def remove_product(self, product):
+        self.products.remove(product)
+
+    def list_products(self):
+        return self.products
+def get_all_categories(connection):
+    category_list = []
+    cursor = connection.cursor()
+    query = """ SELECT * FROM categories"""
+    cursor.execute(query)
+    columns = [column[0] for column in cursor.description]
+    for c in cursor.fetchall():
+        category_list.append(Category(**dict(zip(columns, c))))
+    return category_list
